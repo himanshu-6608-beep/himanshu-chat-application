6ef24a2cde6c4173772614fee67635ca1db9f2cb
Himanshu turpani <himanshuturpani889900@gmail.com>
2026-08-06 12:38:55 +0530

    o

--- backend/routes/socketRoutes.js
+++
@@ -1,8 +0,0 @@
-import {getMessages} from "../controllers/messageController.js";
-import express from "express";
-
-const router = express.Router();
-
-router.get("/messages/:senderId/:receiverId", getMessages);
-
-export default router
